e955c61eb722c169264636bb0b27b843d2f43626
Maniax <39648984+maniax-jp@users.noreply.github.com>
2023-04-25 02:39:00 +0900

    TOP

--- keyboard.py
+++ keyboard.py
@@ -2,14 +2,18 @@
 
 from sys import argv
 import pyautogui
+import time
 
 argc = len(argv)
 
-if argc == 2:
-    pyautogui.press(argv[1])
+if argc >= 2:
+    time.sleep(float(argv[1]))
 
 if argc == 3:
-    pyautogui.hotkey(argv[1], argv[2], interval=0.1)
+    pyautogui.press(argv[2])
 
 if argc == 4:
-    pyautogui.hotkey(argv[1], argv[2], argv[3], interval=0.1)
+    pyautogui.hotkey(argv[2], argv[3], interval=0.1)
+
+if argc == 5:
+    pyautogui.hotkey(argv[2], argv[3], argv[4], interval=0.1)
