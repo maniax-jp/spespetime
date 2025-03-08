4fc33f986f5cd712a6a047fc41d284d497e97881
Maniax <39648984+maniax-jp@users.noreply.github.com>
2025-03-08 14:05:55 +0900

    refactor

--- keyboard.py
+++ keyboard.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
 
+"""
+引数に従ってキーボード操作を行う
+引数1: 待機時間(秒)
+引数2: キー
+引数3: キー
+引数4: キー
+
+例1: 0.1秒待ってEnterキーを押す
+$ python3 keyboard.py 0.1 Enter
+
+例2: 0.1秒待ってCtrl + Shift + F1を押す
+$ python3 keyboard.py 0.1 ctrl shift f1
+"""
+
 from sys import argv
-import pyautogui
 import time
 
-argc = len(argv)
+import pyautogui
 
-if argc >= 2:
+ARGC = len(argv)
+
+if ARGC >= 2:
     time.sleep(float(argv[1]))
 
-if argc == 3:
+if ARGC == 3:
     pyautogui.press(argv[2])
 
-if argc == 4:
+if ARGC == 4:
     pyautogui.hotkey(argv[2], argv[3], interval=0.1)
 
-if argc == 5:
+if ARGC == 5:
     pyautogui.hotkey(argv[2], argv[3], argv[4], interval=0.1)
